71bc364a9abc670addf27694940cd05874564388
FRedwuSong <diadora1166@gmail.com>
2023-09-12 15:11:54 +0800

    Create Company model

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_911_021_024) do
+ActiveRecord::Schema[7.0].define(version: 20_230_912_055_931) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'companies', force: :cascade do |t|
+    t.string 'name'
+    t.boolean 'active'
+    t.integer 'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
 
   create_table 'users', force: :cascade do |t|
     t.string 'fullname', default: '', null: false
