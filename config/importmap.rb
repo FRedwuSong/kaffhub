faaea8e268a5821192fc1ec1afb43efc4881f8ac
FRedwuSong <diadora1166@gmail.com>
2023-09-19 08:07:03 +0800

    install stimulus-read-more use import maps

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+# pin 'stimulus-read-more', to: 'stimulus-read-more', preload: true
+pin 'stimulus-read-more', to: 'https://ga.jspm.io/npm:stimulus-read-more@4.1.0/dist/stimulus-read-more.mjs'
